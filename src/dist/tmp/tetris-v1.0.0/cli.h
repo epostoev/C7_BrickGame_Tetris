b44f194fe8da84ca4716041102ba9ac0586c6eb6
lharysen <lharysen@student.21-school.ru>
2025-12-12 11:59:17 +0300

    edit MakeFile

---
+++ src/dist/tmp/tetris-v1.0.0/cli.h
@@ -0,0 +1,16 @@
+#ifndef CLI_H
+#define CLI_H
+#include <ncurses.h>
+
+#include "../../brick_game/brick_game.h"
+
+#define KEY_P_LOWER 112
+#define KEY_Q_LOWER 113
+#define KEY_SPACE 32
+
+void initNcurses(void);
+void gameLoop(void);
+bool getAction(UserAction_t *key_action);
+void drawStateInfo(GameInfo_t state_info);
+
+#endif // CLI_H
---
+++ src/dist/tmp/tetris-v1.0.0/tetris.h
@@ -0,0 +1,69 @@
+#ifndef TETRIS_H
+#define TETRIS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#include "../brick_game.h"
+
+typedef struct {
+  int cnt1;
+  int cnt2;
+} My_Counter;
+
+typedef enum {
+  kStart,
+  kPause,
+  kMove,
+  kGameOver,
+} FiniteState_t;
+
+typedef struct {
+  int** field;
+  int** next;
+  int score;
+  int high_score;
+  int level;
+  int speed;
+  int** current;
+  int x;
+  int y;
+  FiniteState_t fsm;
+  unsigned long last_tick;
+  unsigned long update_interval;
+  int pause;
+
+  // статистика игры
+  int lines_cleared;
+} TetrisState_t;
+
+typedef enum {
+  figure_I,
+  figure_O,
+  figure_L,
+  figure_J,
+  figure_S,
+  figure_T,
+  figure_Z,
+  count_figure,
+} TypeFigure_t;
+
+GameInfo_t getGameInfo(void);
+
+unsigned long currentTimeMs(void);
+bool timeToShift(void);
+bool moveFigureDown(void);
+void clearCurrent(void);
+void addCurrentInField(void);
+void generateFigure(void);
+bool isPointOutField(int x, int y);
+bool canPlaceAt(const TetrisState_t* state, int nx, int ny);
+void rotateFigure(void);
+void moveFigureLeft(void);
+void moveFigureRight(void);
+TetrisState_t* getTetrisInfo(void);
+// Добавать обьявление функций
+
+#endif // TETRIS_H
