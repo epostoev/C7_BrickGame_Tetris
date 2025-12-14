ec75f346a151916179ffde29d05fad898ae2600c
Lharys Endon <lharysen@ox-i5.msk.21-school.ru>
2025-12-14 18:59:08 +0300

    sutecase

--- src/tests/test_tetris.c
+++ src/tests/test_tetris.c
@@ -1,7 +1,30 @@
-#include "../brick_game/tetris/tetris.h"
-#include "../brick_game/brick_game.h"
 #include <check.h>
 
+#include "../brick_game/brick_game.h"
+#include "../brick_game/tetris/tetris.h"
+
+// ==================================================
+// Helper function
+// ==================================================
+  void setup(void) {
+    
+  }
+  void teardown(void) {
+    
+  }
+
+
+// ==================================================
+// Test Suite 1: getTetrisInfo - Иницилизация
+// ==================================================
+START_TEST(test_getTetrisInfo_inital_state) {
+  // Arrange
+  TetrisState_t* state = getTetrisInfo();
+  // Act
+  // Asset
+  ck_assert_ptr_nonnull(state);
+}
+END_TEST
 
 // 1. Tестируем userInput получение сигнала Start
 // Ожидаемое поведение (ОП):
@@ -10,17 +33,15 @@
 // Появление фигуры в поле Current
 // Статискика установлена в ноль (level == 0, speed == 0, score == 0 )
 
-START_TEST(test_name)
-{
+START_TEST(test_name) {
   // Arrange
   UserAction_t key_action = Start;
   bool hold = true;
   TetrisState_t *state = getTetrisInfo();
-	// Act
+  // Act
   userInput(key_action, hold);
   // Assert
   ck_assert_int_eq(state->fsm, kMove);
-
 }
 END_TEST
 
@@ -29,37 +50,75 @@ END_TEST
 // 4. Написать ожидаемое поведение для каждого состояния
 
 
+// ==================================================
+// Suite Creation
+// ==================================================
+Suite *tetris_init_suite(void) {
+  Suite *suite = suite_create("Tetris: Initilization");
+  // Набор разбивается на группы тестов, разделённых по каким-либо критериям.
+  TCase *tcase_core = tcase_create("Core");
+
+  // Добавление теста в группу тестов.
+  tcase_add_checked_fixture(tcase_core, setup, teardown);
+  tcase_add_test(tcase_core, test_getTetrisInfo_inital_state);
+
+  // Добавление теста в тестовый набор.
+  suite_add_tcase(suite, tcase_core);
+
+  return suite;
+}
+
 // Функция создания набора тестов.
-Suite *example_suite_create(void)
-{
-    Suite *suite = suite_create("Example");
-    // Набор разбивается на группы тестов, разделённых по каким-либо критериям.
-    TCase *tcase_core = tcase_create("Core of example");
-    
-    // Добавление теста в группу тестов.
-    tcase_add_test(tcase_core, test_name);
-    
-    // Добавление теста в тестовый набор.
-    suite_add_tcase(suite, tcase_core);
-    
-    return suite;
+Suite *example_suite_create(void) {
+  Suite *suite = suite_create("Example");
+  // Набор разбивается на группы тестов, разделённых по каким-либо критериям.
+  TCase *tcase_core = tcase_create("Core of example");
+
+  // Добавление теста в группу тестов.
+  tcase_add_test(tcase_core, test_name);
+  tcase_add_test(tcase_core, test_name);
+
+  // Добавление теста в тестовый набор.
+  suite_add_tcase(suite, tcase_core);
+
+  return suite;
 }
 
-int main(void)
-{
-    Suite *suite = example_suite_create();
-    SRunner *suite_runner = srunner_create(suite);
-    
-    srunner_run_all(suite_runner, CK_NORMAL);
-    // Получаем количество проваленных тестов.
-    int failed_count = srunner_ntests_failed(suite_runner);
-    srunner_free(suite_runner);
+int main(void) {
+  int number_field;
+  SRunner *suite_runner;
 
-    int result_test = EXIT_SUCCESS;
-    if (failed_count != 0) {
-        // Сигнализируем о том, что тестирование прошло неудачно.
-        result_test = EXIT_FAILURE;
-    }
+  suite_runner = srunner_create(tetris_init_suite());
 
-    return result_test;
+  srunner_run_all(suite_runner, CK_VERBOSE);
+
+  // Получаем количество проваленных тестов.
+  int failed_count = srunner_ntests_failed(suite_runner);
+  srunner_free(suite_runner);
+
+  int result_test = EXIT_SUCCESS;
+  if (failed_count != 0) {
+    // Сигнализируем о том, что тестирование прошло неудачно.
+    result_test = EXIT_FAILURE;
+  }
+
+  return result_test;
 }
+// int main(void) {
+//   int number_field;
+//   Suite *suite = example_suite_create();
+//   SRunner *suite_runner = srunner_create(suite);
+
+//   srunner_run_all(suite_runner, CK_NORMAL);
+//   // Получаем количество проваленных тестов.
+//   int failed_count = srunner_ntests_failed(suite_runner);
+//   srunner_free(suite_runner);
+
+//   int result_test = EXIT_SUCCESS;
+//   if (failed_count != 0) {
+//     // Сигнализируем о том, что тестирование прошло неудачно.
+//     result_test = EXIT_FAILURE;
+//   }
+
+//   return result_test;
+// }
